2d879ad7751d30921c4edf0285f08d331539bd66
AhmedBoustani <ahmed.a.boustani@gmail.com>
2016-05-21 00:03:13 -0700

    Create Python_in_C.c

---
+++ Python_in_C.c
@@ -0,0 +1,16 @@
+/*
+ * This program is taken from Python Docs
+ * It's used to run Python programs in C
+*/
+
+#include <Python.h>
+
+int main(int argc, char **argv) {
+  Py_SetProgramName(argv[0]);  /* optional but recommended */
+  Py_Initialize();
+  PyRun_SimpleString("from time import time,ctime\n"
+                     "print 'Today is',ctime(time())\n");
+  // or used PyRun_SimpleFile() to run a program file
+  Py_Finalize();
+  return 0;
+}
